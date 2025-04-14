fb0fae017886fe1245d1f15564a4b941f0c08dd0
Affan <affandeveloper3424@gmail.com>
2025-04-14 19:49:46 +0530

    imrpoved resposiveness

--- test/test.js
+++ test/test.js
@@ -1,5 +1,9 @@
-const arr = [1, 2, 3, 4, 5, 6]
-const i = 2
-console.log([...arr.slice(0, i), "two", ...arr.slice(i)]);
-
-
+function returnPromise(){
+  return new Promise((resolve, reject) =>{
+    setTimeout(() => {
+      resolve("resolvedoo")
+    }, 2000);
+  })
+}
+console.log(returnPromise());
+console.log("other");
